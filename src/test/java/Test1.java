86b2de9e4953c130852f09dbbd12fb67491d76d0
Mrunal2148 <mkapure@asu.edu>
2025-04-01 15:25:09 -0700

    added test and java file

---
+++ src/test/java/Test1.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test1 {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test2 {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
